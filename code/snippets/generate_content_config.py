f2b020149212d898617bb847abf4ed5a668f698e
TheSeeker <launchpadllc2024@gmail.com>
2025-12-26 00:50:54 -0500

    Resonant Horizon: Decomposed generate_content_config.py

---
+++ code/snippets/generate_content_config.py
@@ -0,0 +1,6 @@
+generate_content_config = types.GenerateContentConfig(
+    thinking_config=types.ThinkingConfig(
+        thinking_level="HIGH",
+    ),
+    system_instruction=[
+        types.Part.from_text(text="""{
